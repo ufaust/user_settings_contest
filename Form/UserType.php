09a937825ec051b701c1d8dd89c4faa6bf39cfb0
Timur <timurdu@yandex.ru>
2023-09-25 20:12:30 +0500

    Delete IDE annotations

--- Form/UserType.php
+++ Form/UserType.php
@@ -3,11 +3,9 @@
 namespace App\Form;
 
 use App\Entity\User;
-use JetBrains\PhpStorm\ArrayShape;
 
 class UserType
 {
-    #[ArrayShape(['data_class' => "string"])]
     public function options(): array
     {
         return ['data_class' => User::class];
